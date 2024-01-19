045480c5d97ed909d7fb8a3306bc6942ba43ec56
ptSouthier <patrick.southier@hotmail.com>
2024-01-19 14:14:39 -0300

    feat: creating NoteDrawer component file

---
+++ components/NoteDrawer.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-drawer show-if-above v-model="drawerSwitch" side="left" behavior="desktop" bordered class="note-drawer-bg column">
+    <div class="row items-start q-mt-md">
+      <p class="col-10 text-h6 text-bold text-grey-7 q-pl-md">
+        Anotações
+      </p>
+      <q-btn
+        flat
+        round
+        color="grey-5"
+        icon="close"
+      />
+    </div>
+    <div class="col absolute-center text-center">
+      <q-icon
+        name="attach_file"
+        size="50px"
+        class="text-grey-6 rotate-225 q-mb-md"
+      />
+
+      <p class="text-h6 text-bold text-blue-6 q-mb-none">
+        Anotações
+      </p>
+
+      <p class="text-grey-6 by-speedio">
+        By Speedio
+      </p>
+
+      <q-btn
+        rounded
+        color="blue-6"
+        label="+ Criar anotação"
+        no-caps
+        class="q-mt-md"
+      />
+    </div>
+  </q-drawer>
+</template>
+
+<script setup lang="ts">
+  const drawerSwitch = defineModel<boolean>('drawerSwitch');
+</script>
+
+<style>
+.note-drawer-bg {
+  background-color: #e3f1f7 !important;
+}
+
+.text-h6 {
+  letter-spacing: -0.02rem;
+}
+
+.by-speedio {
+  margin-top: -0.5rem;
+  font-size: small;
+}
+</style>
