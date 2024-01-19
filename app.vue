4bca4f2ae41e88a398656df15108aa0bf499ce31
ptSouthier <patrick.southier@hotmail.com>
2024-01-19 11:39:13 -0300

    feat: creating a layout view with header and drawer at app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar square>
+            <img src="https://speedio.com.br/assets/img/speedio-favicon.svg"> <!-- TODO: Criar ícone SVG SpeedIO -->
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <NoteDrawer v-model:drawer-switch="isDrawerOpen" />
+
+  </q-layout>
 </template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const isDrawerOpen = ref(false);
+
+  const toggleDrawer = () => {
+    isDrawerOpen.value = !isDrawerOpen.value
+  };
+</script>
