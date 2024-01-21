8a7cd5f92a21593661a3896a5c22e1b5cc168408
ptSouthier <patrick.southier@hotmail.com>
2024-01-20 22:20:01 -0300

    refac: moving NoteDrawer component into noteDrawer folder

---
+++ components/noteDrawer/NoteDrawer.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-drawer show-if-above v-model="drawerController" side="left" behavior="desktop" bordered class="note-drawer-bg column">
+
+    <div class="row items-start q-mt-md">
+      <p class="col-10 text-h6 text-bold text-grey-7 q-pl-md">
+        Anotações
+      </p>
+      <q-btn
+        flat
+        round
+        color="grey-5"
+        icon="close"
+        @click="handleDrawerClosage"
+      />
+    </div>
+
+    <NoteDrawerEmpty
+      v-model:create-note="isCreateNoteOpen"
+      v-if="!noteList.length && !isCreateNoteOpen"
+    />
+
+    <CreateNote
+      v-if="isCreateNoteOpen == true"
+    />
+
+  </q-drawer>
+</template>
+
+<script setup lang="ts">
+  const drawerController = defineModel<boolean>('drawerSwitch');
+  const isCreateNoteOpen = ref<boolean>(false);
+  const noteList = ref<Array<Note>>([]);
+  
+  interface Note {
+    description: string,
+    businessPotential: number,
+    category: Array<string>,
+    reminder: Date,
+  }
+
+  const emits = defineEmits<{
+    closeDrawer: [value: boolean],
+  }>();
+
+  const handleDrawerClosage = (): void => {
+    emits('closeDrawer', false);
+  };
+</script>
+
+<style>
+.note-drawer-bg {
+  background-color: #e3f1f7 !important;
+}
+
+.text-h6 {
+  letter-spacing: -0.02rem;
+}
+
+.by-speedio {
+  margin-top: -0.5rem;
+  font-size: small;
+}
+</style>
