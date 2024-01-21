132ddd19243ede991b9fe3ca924c9d75407cfa36
ptSouthier <patrick.southier@hotmail.com>
2024-01-20 22:21:23 -0300

    feat: creating NoteDrawerEmpty component

---
+++ components/noteDrawer/NoteDrawerEmpty.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col absolute-center text-center">
+    <q-icon
+      name="attach_file"
+      size="3.13rem"
+      class="text-grey-6 rotate-225 q-mb-md"
+    />
+
+    <p class="text-h6 text-bold text-blue-6 q-mb-none">
+      Anotações
+    </p>
+
+    <p class="text-grey-6 by-speedio">
+      By Speedio
+    </p>
+
+    <q-btn
+      rounded
+      color="blue-6"
+      label="+ Criar anotação"
+      no-caps
+      class="q-mt-md"
+      @click="openCreateNote"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  const creatingNote = defineModel<boolean>('createNote');
+
+  const openCreateNote = (): void => {
+    creatingNote.value = true;
+  };
+</script>
+
+<style>
+</style>
