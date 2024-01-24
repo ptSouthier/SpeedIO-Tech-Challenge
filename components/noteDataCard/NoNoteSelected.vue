d91a5066dc44d4a492cd50b305af7f97561e66df
ptSouthier <patrick.southier@hotmail.com>
2024-01-24 11:22:48 -0300

    feat: creating NoNoteSelected component inside noteDataCard folder

---
+++ components/noteDataCard/NoNoteSelected.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-h6 q-my-md q-px-sm">
+          Selecione no menu à esquerda uma de suas <span class="text-blue-2 text-bold">Anotações</span>
+          criadas para visualizar suas informações detalhadas aqui!
+        </div>
+
+        <q-separator style="min-width: 100%;" />
+
+        <div class="text-subtitle2 q-my-md q-px-sm">
+          Se este é seu primeiro acesso, abra o menu posicionado ao lado esquerdo e comece criando as Anotações importantes para seu dia a dia! <br><br>
+          Após a criação de sua Anotação, será possível salvar sua informações através do botão "Persistir Meus Dados". <br><br>
+          Caso já tenha criado e persistido suas Anotações em alguma sessão anterior, 
+          é possível restaurá-las usando o Token gerado no momento do upload dos dados, clicando em "Restaurar Meus Dados".
+        </div>
+</template>
