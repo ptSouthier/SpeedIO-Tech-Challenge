5408a85f241fd220dfac509d88191f42475e0439
ptSouthier <patrick.southier@hotmail.com>
2024-01-24 07:10:59 -0300

    feat: creating the DeleteNote modal component and its logics

---
+++ components/modals/DeleteNote.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-dialog v-model="showModal" persistent>
+    <q-card style="border-radius: 0.5rem;">
+      <q-card-section class="row items-center q-pb-none">
+        <div class="text-h6 text-grey-10 q-pa-sm q-mb-sm">Deseja excluir essa anotação</div>
+        <q-space />
+        <q-btn
+          class="q-mb-sm"
+          color="grey-5"
+          icon="sym_o_close"
+          flat
+          round
+          dense
+          v-close-popup
+        />
+      </q-card-section>
+
+      <q-separator style="min-width: 100%;" />
+
+      <q-card-section class="row items-center">
+        <span class="text-grey-8 q-mx-md q-mb-lg">
+          Você está prestes a deletar uma anotação e esta ação não poderá ser desfeita.
+          <br>Tem certeza que deseja excluí-la?
+        </span>
+      </q-card-section>
+
+      <q-card-actions align="right" class="q-pr-md q-pb-md">
+        <q-btn
+          outline
+          rounded
+          no-caps
+          label="Cancelar"
+          text-color="grey-8"
+          padding="0.4rem 0.8rem"
+          class="q-mr-xs"
+          v-close-popup
+        />
+        <q-btn
+          unelevated
+          rounded
+          no-caps
+          label="Excluir"
+          color="red"
+          text-color="white"
+          padding="0.4rem 0.8rem"
+          @click="handleNoteDeletion"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+  import type { Note } from '../noteDrawer/NoteDrawer.types';
+  import { Loading } from 'quasar';
+
+  const showModal = defineModel<boolean>('showModal');
+  const notesList = defineModel<Array<Note>>('notesList');
+  const noteId = defineModel<number>('noteId');
+
+  const emits = defineEmits<{
+    closeModal: [value: boolean],
+  }>();
+
+  const handleNoteDeletion = async (): Promise<void> => {
+    Loading.show();
+
+    if (noteId.value !== undefined) {
+      const deleteNoteData = await useIndexDB.deleteNote(noteId.value);
+      
+      if (deleteNoteData.status === HTTP_STATUS_NO_CONTENT) {        
+        notesList.value = notesList.value?.filter((note: any) => note.id !== noteId.value);
+        emits('closeModal', false);
+      };
+    };
+      
+    Loading.hide();
+  };
+</script>
