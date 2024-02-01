ec362f0bc193c3809404c4a8bc9ffabf4665130f
ptSouthier <patrick.southier@hotmail.com>
2024-02-01 20:19:39 -0300

    refac: implementing the created modals and its logics into NoteDataCard component

--- components/noteDataCard/NoteDataCard.vue
+++ components/noteDataCard/NoteDataCard.vue
@@ -11,14 +11,30 @@
     </q-card-section>
 
     <q-card-actions align="around">
-      <q-btn flat text-color="green-5" class="text-bold" icon="sym_o_download" align="around">Restaurar Meus Dados</q-btn>
-      <q-btn flat text-color="blue-5" class="text-bold" icon="sym_o_upload" align="around">Persistir Meus Dados</q-btn>
+      <q-btn flat text-color="green-5" class="text-bold" icon="sym_o_download" align="around" @click="(() => openInsertTokenModal = true)">
+        Restaurar Meus Dados
+      </q-btn>
+
+      <q-btn flat text-color="blue-5" class="text-bold" icon="sym_o_upload" align="around" @click="(() => openPersistNotesModal = true)">
+        Persistir Meus Dados
+      </q-btn>
     </q-card-actions>
   </q-card>
+
+  <InsertTokenToRetrieve
+    v-model:show-token-modal="openInsertTokenModal"
+  />
+
+  <ConfirmPersistNotes
+    v-model:show-persist-modal="openPersistNotesModal"
+  />
+
 </template>
 
 <script setup lang="ts">
   import type { Note } from '../noteDrawer/NoteDrawer.types';
 
   const selectedNote = defineModel<Note>('selectedNote');
+  const openInsertTokenModal = ref<boolean>(false);
+  const openPersistNotesModal = ref<boolean>(false);
 </script>
